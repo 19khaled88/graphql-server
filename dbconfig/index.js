4bb88ede8599ff05f72374aebed37dbe2c45228b
19khaled88 <69922782+19khaled88@users.noreply.github.com>
2022-07-15 00:06:13 +0600

    config heroku server

--- dbconfig/index.js
+++ dbconfig/index.js
@@ -1,8 +1,10 @@
 const MongoClient = require('mongodb').MongoClient
 
 //Connection URL
-
-const url = 'mongodb://localhost:27017/'
+const name = process.env.APP_USER_NAME
+const pass = process.env.APP_USER_PASS
+const url = `mongodb+srv://${name}:${pass}@cluster0.ka5da.mongodb.net/graphql?retryWrites=true&w=majority`
+// const url = 'mongodb://localhost:27017/'
 const client = new MongoClient(url, { useUnifiedTopology: true })
 client.connect(function (err) {
   console.log('Connection successful to server')
